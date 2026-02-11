13b0d771b990fddda449a86e5a7dbd83c85f0a5d
Maha-Gabr <mahagabr523@gmail.com>
2026-02-11 21:33:40 +0300

    add app routes file

---
+++ lib/utiles/app_routes.dart
@@ -0,0 +1,8 @@
+class AppRoutes {
+  static final String homeRouteName = '/home_screen';
+  static final String logicRouteName = '/login_screen';
+  static final String registerRouteName = '/register_screen';
+  static final String addEventRouteName = '/add_event_screen';
+
+
+}
