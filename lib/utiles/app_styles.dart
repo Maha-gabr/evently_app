0b8ec36b65dabd689012d5f31503b2e63a6060a8
Maha-Gabr <mahagabr523@gmail.com>
2026-02-11 21:37:20 +0300

    add app styles file

--- lib/utiles/app_styles.dart
+++ lib/utiles/app_styles.dart
@@ -2,33 +2,74 @@
 import 'package:evently_app/utiles/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class AppStyles {
   static TextStyle semi20Black =GoogleFonts.poppins(
       fontSize: 20,
-      fontWeight: FontWeight.w700,
+      fontWeight: FontWeight.w600,
       color: AppColors.blackColor);
 
   static TextStyle semi20White =GoogleFonts.poppins(
       fontSize: 20,
-      fontWeight: FontWeight.w700,
+      fontWeight: FontWeight.w600,
       color: AppColors.whiteColor);
+  static TextStyle semi14White =GoogleFonts.poppins(
+      fontSize: 14,
+      fontWeight: FontWeight.w600,
+      color: AppColors.whiteColor);
+  static TextStyle semi14Main =GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    color: AppColors.mainColor,
+    decoration: TextDecoration.underline,
+    decorationColor: AppColors.mainColor,
+    decorationThickness: 2,
+  );
+  static TextStyle semi14MainDark =GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: AppColors.mainDarkColor,
+    decoration: TextDecoration.underline,
+    decorationColor: AppColors.mainDarkColor,
+    decorationThickness: 2,
+  );
+  static TextStyle semi16Main =GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: AppColors.mainColor,
 
+  );
+  static TextStyle semi16MainDark=GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: AppColors.mainDarkColor,
+
+  );
   static TextStyle reg14White =GoogleFonts.poppins(
       fontSize: 14,
       fontWeight: FontWeight.w400,
       color: AppColors.whiteDarkColor);
-
   static TextStyle reg14Grey =GoogleFonts.poppins(
       fontSize: 14,
       fontWeight: FontWeight.w400,
       color: AppColors.greyColor);
+  static TextStyle reg14whiteDark =GoogleFonts.poppins(
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      color: AppColors.whiteDarkColor);
 
   static TextStyle med16Black =GoogleFonts.poppins(
       fontSize: 16,
       fontWeight: FontWeight.w600,
       color: AppColors.blackColor);
+  static TextStyle med16main =GoogleFonts.poppins(
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+      color: AppColors.mainColor);
+  static TextStyle med16mainDark =GoogleFonts.poppins(
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+      color: AppColors.mainDarkColor);
   static TextStyle med16White =GoogleFonts.poppins(
       fontSize: 16,
       fontWeight: FontWeight.w600,
@@ -37,5 +78,51 @@ class AppStyles {
       fontSize: 20,
       fontWeight: FontWeight.w700,
       color: AppColors.mainColor);
+  static TextStyle reg12grey =GoogleFonts.poppins(
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      color: AppColors.lightGreyColor);
+  static TextStyle reg12main =GoogleFonts.poppins(
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      color: AppColors.mainColor);
+  static TextStyle reg12mainDark =GoogleFonts.poppins(
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      color: AppColors.mainDarkColor);
+
+  static TextStyle semi24main =GoogleFonts.poppins(
+      fontSize: 24,
+      fontWeight: FontWeight.w700,
+      color: AppColors.mainColor);
+  static TextStyle semi24white =GoogleFonts.poppins(
+      fontSize: 24,
+      fontWeight: FontWeight.w700,
+      color: AppColors.whiteColor);
+  static TextStyle med20white =GoogleFonts.poppins(
+      fontSize: 20,
+      fontWeight: FontWeight.w500,
+      color: AppColors.whiteColor);
+  static TextStyle med20black =GoogleFonts.poppins(
+      fontSize: 20,
+      fontWeight: FontWeight.w500,
+      color: AppColors.blackColor);
+  static TextStyle med18main =GoogleFonts.poppins(
+      fontSize: 18,
+      fontWeight: FontWeight.w500,
+      color: AppColors.mainColor);
+  static TextStyle med18mainDark =GoogleFonts.poppins(
+      fontSize: 18,
+      fontWeight: FontWeight.w500,
+      color: AppColors.mainDarkColor);
+  static TextStyle med14Black =GoogleFonts.poppins(
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+      color: AppColors.blackColor);
+  static TextStyle med14White =GoogleFonts.poppins(
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+      color: AppColors.whiteColor);
+
 
 }
