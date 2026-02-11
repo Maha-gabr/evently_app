48403e94c1df567eaacb691a9cf1af72812390f8
Maha-Gabr <mahagabr523@gmail.com>
2026-02-11 22:50:51 +0300

    feat:implement bottom navigation bar for root screen part 1

---
+++ lib/ui/root_screen.dart
@@ -0,0 +1,38 @@
+import 'package:evently_app/ui/tabs/profile_tap/profile_tab.dart';
+import 'package:flutter/material.dart';
+
+import '../l10n/app_localizations.dart';
+class  RootScreen extends StatelessWidget {
+  const  RootScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // List <Widget> widgetsList = [
+    //   // HomeTap(),
+    //   // FavoriteTap(),
+    //   ProfileTap(),
+    // ];
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+          destinations:[
+            NavigationDestination(
+              icon: Icon(Icons.home_outlined),
+              selectedIcon: Icon(Icons.home_filled),
+              label: AppLocalizations.of(context)!.home
+          ) ,
+            NavigationDestination(
+                icon: Icon(Icons.favorite_border),
+                selectedIcon: Icon(Icons.favorite),
+                label: AppLocalizations.of(context)!.favorite
+            ) ,
+            NavigationDestination(
+                icon: Icon(Icons.person),
+                selectedIcon: Icon(Icons.home),
+                label: AppLocalizations.of(context)!.profile
+            ) ,
+          ]
+
+      ),
+    );
+  }
+}
