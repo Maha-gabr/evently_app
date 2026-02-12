6bfe0cd5f7eeeac4b33322adc4a0146fcf3739f3
Maha-Gabr <mahagabr523@gmail.com>
2026-02-12 20:05:23 +0300

    feat: rename collectionRef to eventCollectionRef

--- lib/utiles/firebase_utils.dart
+++ lib/utiles/firebase_utils.dart
@@ -3,14 +3,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/event.dart';
 class FirebaseUtils {
 
-static CollectionReference<Event> collectionRef(){
+static CollectionReference<Event> eventCollectionRef(){
   return FirebaseFirestore.instance.collection(Event.collectionName).withConverter<Event>(
       fromFirestore: (snapshot, options) => Event.fromFireStore(snapshot.data()!),
       toFirestore: (value, options) => value.toFireStore(),);
 }
 
 static Future<void> addEventToFireStore(Event event){
-  DocumentReference<Event> docRef = collectionRef().doc();
+  DocumentReference<Event> docRef = eventCollectionRef().doc();
   event.id =docRef.id;
   return docRef.set(event);
 }
