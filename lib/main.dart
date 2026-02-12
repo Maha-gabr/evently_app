54b3cfec3c0445a1b1ae618840498e0d6cc3e8bc
Maha-Gabr <mahagabr523@gmail.com>
2026-02-12 06:29:41 +0300

    feat: integrate firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,17 @@ import 'package:evently_app/ui/root_screen.dart';
 import 'package:evently_app/ui/tabs/home_tap/home_tap.dart';
 import 'package:evently_app/utiles/app_routes.dart';
 import 'package:evently_app/utiles/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 import 'l10n/app_localizations.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => AppLangProvider(),),
