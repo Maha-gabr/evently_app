58dc4f4ad5bd1ee2d30e233702c8405e6b4ec57e
Maha-Gabr <mahagabr523@gmail.com>
2026-02-12 05:17:10 +0300

    add app assets file

--- lib/utiles/app_assets.dart
+++ lib/utiles/app_assets.dart
@@ -1,4 +1,16 @@
-import 'package:flutter/material.dart';
 class AppAssets {
   static final String routeLogo = 'assets/images/profile_pic.png';
+  static final String sport ='assets/images/light_Sport.png';
+
+  static final String darkSport ='assets/images/dark_sport.png';
+
+  static final String birthday ='assets/images/light_Birthday.png';
+  static final String darkBirthday ='assets/images/dark_Birthday.png';
+  static final String exhibtion ='assets/images/light_Exhibition.png';
+  static final String darkExhipition ='assets/images/dark_Exhibition.png';
+  static final String meeting ='assets/images/light_Meeting.png';
+  static final String darkMeeting ='assets/images/dark_Meeting.png';
+  static final String bookClub ='assets/images/light_Book Club.png';
+  static final String darkBookClub ='assets/images/dark_Book Club.png';
+
 }
