9d38c2637dd38dc92441c69f56333f0cfbf16438
Maha-Gabr <mahagabr523@gmail.com>
2026-02-18 18:24:19 +0300

    feat: add register screen ui

--- lib/utiles/app_assets.dart
+++ lib/utiles/app_assets.dart
@@ -1,9 +1,9 @@
 class AppAssets {
   static final String routeLogo = 'assets/images/profile_pic.png';
   static final String sport ='assets/images/light_Sport.png';
-
+  static final String logoImage ='assets/images/logo_image.png';
   static final String darkSport ='assets/images/dark_sport.png';
-
+  static final String emailIcon ='assets/images/email_icon.png';
   static final String birthday ='assets/images/light_Birthday.png';
   static final String darkBirthday ='assets/images/dark_Birthday.png';
   static final String exhibtion ='assets/images/light_Exhibition.png';
