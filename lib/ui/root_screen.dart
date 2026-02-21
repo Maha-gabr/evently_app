d9b9c18c8ce63aa56cc10bc02d60550f32487916
Maha-Gabr <mahagabr523@gmail.com>
2026-02-22 00:53:47 +0300

    feat: remove unnecessary code in root screen

--- lib/ui/root_screen.dart
+++ lib/ui/root_screen.dart
@@ -2,7 +2,6 @@ import 'package:evently_app/ui/tabs/fav_tap/fav_tap.dart';
 import 'package:evently_app/ui/tabs/home_tap/home_tap.dart';
 import 'package:evently_app/ui/tabs/profile_tap/profile_tab.dart';
 import 'package:flutter/material.dart';
-
 import '../l10n/app_localizations.dart';
 class  RootScreen extends StatefulWidget {
   const  RootScreen({super.key});
@@ -23,7 +22,9 @@ class _RootScreenState extends State<RootScreen> {
     return Scaffold(
       body: Column(
         children: [
-          Expanded(child: tabsList[selectedIndex]),
+          Expanded(
+              child: tabsList[selectedIndex],
+          ),
         ],
       ),
       bottomNavigationBar: NavigationBar(
@@ -39,7 +40,7 @@ class _RootScreenState extends State<RootScreen> {
               icon: Icon(Icons.home_outlined),
               selectedIcon: Icon(Icons.home_filled),
               label: AppLocalizations.of(context)!.home
-          ) ,
+                      ) ,
             NavigationDestination(
                 icon: Icon(Icons.favorite_border),
                 selectedIcon: Icon(Icons.favorite),
