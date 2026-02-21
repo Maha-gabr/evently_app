46a5c747faa4337ae615491b4918994f06784107
Maha-Gabr <mahagabr523@gmail.com>
2026-02-22 00:48:37 +0300

    feat: add app toast util

--- lib/utiles/firebase_utils.dart
+++ lib/utiles/firebase_utils.dart
@@ -1,17 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../models/event.dart';
+import '../models/user.dart';
 class FirebaseUtils {
 
-static CollectionReference<Event> eventCollectionRef(){
-  return FirebaseFirestore.instance.collection(Event.collectionName).withConverter<Event>(
+static CollectionReference<Event> eventCollectionRef(String uid){
+  return  usersCollectionRef().doc(uid).collection(Event.collectionName).withConverter<Event>(
       fromFirestore: (snapshot, options) => Event.fromFireStore(snapshot.data()!),
       toFirestore: (value, options) => value.toFireStore(),);
 }
-
-static Future<void> addEventToFireStore(Event event){
-  DocumentReference<Event> docRef = eventCollectionRef().doc();
+static Future<void> addEventToFireStore(Event event,String uid){
+  DocumentReference<Event> docRef = eventCollectionRef(uid).doc();
   event.id =docRef.id;
   return docRef.set(event);
+}
+// get user collection
+ static CollectionReference<MyUser>usersCollectionRef(){
+  return FirebaseFirestore.instance.collection(MyUser.collectionName).withConverter(
+      fromFirestore: (snapshot, options) => MyUser.fromJson(snapshot.data()!),
+      toFirestore: (value, options) => value.toJson(),);
+}
+static Future<void> addUsersToFireStore(MyUser user) {
+  return usersCollectionRef().doc(user.id).set(user);
 }
 }
