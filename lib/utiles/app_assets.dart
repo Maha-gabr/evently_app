74313a60db3643f67fbdd3ffcbe3f4dbcaf9ee0c
Maha-Gabr <mahagabr523@gmail.com>
2026-03-03 07:09:24 +0300

    add app_assets and app_routes

--- lib/utiles/app_assets.dart
+++ lib/utiles/app_assets.dart
@@ -12,5 +12,17 @@ class AppAssets {
   static final String darkMeeting ='assets/images/dark_Meeting.png';
   static final String bookClub ='assets/images/light_Book Club.png';
   static final String darkBookClub ='assets/images/dark_Book Club.png';
+  static final String darkLogoImage ='assets/images/logo_dark_image.png';
+  static final String onBoardingImage1= 'assets/images/on_boarding1.png';
+  static final String onBoardingImage2= 'assets/images/on_boarding2.png';
+  static final String onBoardingImage3= 'assets/images/on_boarding3.png';
+  static final String onBoardingImage4= 'assets/images/on_boarding4.png';
+  static final String onBoardingDark1 ='assets/images/onboarding_dark1.png';
+  static final String onBoardingDark2 ='assets/images/onboarding_dark2.png';
+  static final String onBoardingDark3 ='assets/images/onboarding_dark3.png';
+  static final String onBoardingDark4 ='assets/images/onboarding_dark4.png';
+
+
+
 
 }
--- lib/utiles/app_routes.dart
+++ lib/utiles/app_routes.dart
@@ -4,5 +4,7 @@ class AppRoutes {
   static final String routeScreenRouteName = '/root_screen';
   static final String loginScreenRouteName = '/login_screen';
   static final String registerScreenRouteName = '/register_screen';
+  static final String onBoardingScrenRouteName = '/onBoardingScreen';
+
 
 }
